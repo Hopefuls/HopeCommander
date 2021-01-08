3b29ae5a3337a63073ce95e96941b4e56e5ddd16
HopeDev <ballinaurel@gmail.com>
2021-01-08 20:32:20 +0100

    Fixed issue with outside-network requests
    Fixed a flaw with being able to make plugin-channel requests to the bungeecord without being part of the network (Thanks to Maroon#1461 for making me aware of this)

--- src/main/java/me/hopedev/hopecommander/Bungeecord/Bungee.java
+++ src/main/java/me/hopedev/hopecommander/Bungeecord/Bungee.java
@@ -4,12 +4,22 @@ import me.hopedev.hopecommander.Bungeecord.Listeners.PluginMessage;
 import me.hopedev.hopecommander.utils.UniversalUsage;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.plugin.Command;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.ArrayList;
 
 public class Bungee extends UniversalUsage {
     /*
     Made by Aurel (Hope) on 27/10/2020
      */
     private final Object pluginInstance;
+    private Configuration configuration;
 
     public Bungee(Object pluginInstance) {
         this.pluginInstance = pluginInstance;
@@ -28,11 +38,33 @@ public class Bungee extends UniversalUsage {
         instance.getProxy().registerChannel("hope:hopecmdsend");
         instance.getProxy().getPluginManager().registerCommand(instance, getCommand("hopecommander"));
         UniversalUsage.universalUsage = this;
+        try {
+            if (!instance.getDataFolder().exists())
+                instance.getDataFolder().mkdir();
+
+            File file = new File(instance.getDataFolder(), "config.yml");
+
+
+            if (!file.exists()) {
+                try (InputStream in = instance.getResourceAsStream("config.yml")) {
+                    Files.copy(in, file.toPath());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            this.configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(new File(instance.getDataFolder(), "config.yml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public UniversalUsage.UsageType getUsageType() {
         return UsageType.BUNGEE;
+    }
+
+    public Configuration getConfiguration() {
+        return this.configuration;
     }
 
     @Override
--- src/main/java/me/hopedev/hopecommander/Bungeecord/Listeners/PluginMessage.java
+++ src/main/java/me/hopedev/hopecommander/Bungeecord/Listeners/PluginMessage.java
@@ -7,7 +7,15 @@ import me.hopedev.hopecommander.utils.UniversalUsage;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.event.PluginMessageEvent;
 import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
 import net.md_5.bungee.event.EventHandler;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PluginMessage implements Listener {
     /*
@@ -19,7 +27,18 @@ public class PluginMessage implements Listener {
             BungeeMain bungeeMain = (BungeeMain) UniversalUsage.get().getPluginInstance();
             ByteArrayDataInput in = ByteStreams.newDataInput(event.getData());
             String commandToRun = in.readUTF();
+            List<String> iplist = null;
             bungeeMain.getProxy().getConsole().sendMessage(new ComponentBuilder("[HopeCommander] Command received from " + event.getSender().getAddress().getAddress().getHostAddress() + ":" + event.getSender().getAddress().getPort() + "» " + commandToRun).create());
+            try {
+                Configuration configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(new File(bungeeMain.getDataFolder(), "config.yml"));
+                iplist = configuration.getStringList("whitelisted-ips");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (!iplist.contains(event.getSender().getAddress().getAddress().getHostAddress())) {
+                System.out.println("[HopeCommander][WARNING] Request IP "+event.getSender().getAddress().getAddress().getHostAddress()+" IS NOT ON whitelisted-ips. Not Executing command!");
+                return;
+            }
             bungeeMain.getProxy().getConsole().sendMessage(new ComponentBuilder("[HopeCommander] executing...").create());
             //send
             bungeeMain.getProxy().getPluginManager().dispatchCommand(bungeeMain.getProxy().getConsole(), commandToRun);
